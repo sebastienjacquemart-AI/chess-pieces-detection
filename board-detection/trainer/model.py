16f10a4cce554a655d0e28dc97f5342758738387
sebastienjacquemart-AI <sebastien.jacquemart@outlook.com>
2024-04-15 22:08:17 +0200

    line detection

--- board-detection/trainer/model.py
+++ board-detection/trainer/model.py
@@ -1,22 +1,43 @@
 from matplotlib import pyplot as plt
 
 import cv2 as cv
-import math
+import numpy as np
 
 class LineDetection:
     def __init__(self):
         pass
 
-    def canny(self, img):
-        edges = cv.Canny(img,100,200)
+    def gaussian_blur(self, img, kernel_size):
+        blurred_img = cv.GaussianBlur(img,(kernel_size, kernel_size),0)
+        return blurred_img
+
+    def canny(self, img, low_tresh, high_tresh):
+        edges = cv.Canny(img,low_tresh,high_tresh)
         return edges
     
     def canny_pf(self, img):
         return
     
+    def hough(self, img, edges):
+        line_image = np.copy(img) * 0
+
+        lines = cv.HoughLinesP(edges, 1, np.pi/180, 60, minLineLength=300, maxLineGap=70)
+
+        assert lines is not None, "no lines found in the image, check hough params"
+        for line in lines:
+            for x1,y1,x2,y2 in line:
+                cv.line(line_image,(x1,y1),(x2,y2),(255,0,0),5)
+        
+        lines_edges = cv.addWeighted(img, 0.8, line_image, 1, 0)
+
+        return lines_edges
+    
+    def get_intersections():
+        return
+    
     def linking_function(self, edges, p, A):
         def get_omega():
-            return (math.pi/2)*(1/A**(-4))
+            return (np.pi/2)*(1/A**(-4))
 
         def get_t_delta():
             return p*get_omega()
@@ -25,7 +46,7 @@ class LineDetection:
             return (a+b)*get_t_delta()
         
         def merge_segments():
-            
+
             return
         
 
