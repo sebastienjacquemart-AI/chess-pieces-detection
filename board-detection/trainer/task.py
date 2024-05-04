707049ccd96530a67ff815a9c60837db1594ed56
sebastienjacquemart-AI <sebastien.jacquemart@outlook.com>
2024-05-04 22:03:24 +0200

    convex hull3

--- board-detection/trainer/task.py
+++ board-detection/trainer/task.py
@@ -7,6 +7,7 @@ dataset = Dataset("./dataset")
 linedetection = LineDetection()
 
 img = dataset.get_train_image("c7890b749d14d3488066cbdfac4620fd_jpg.rf.76f2ad22e2a1c25bc8df3e234a2875e4.jpg")
+#img = dataset.get_train_image("d494cb268ad7f9f55587de138edc1dc4_jpg.rf.1225ba2bab92010c41fd82111da127c5.jpg")
 
 lines, labels, intersection_points = linedetection.intersections(img)
 
